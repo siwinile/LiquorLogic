a80daf90d474ce2dd5228f6c46abe9efff9c4418
LiquorLogic <hello@liquorlogic.co.za>
2024-05-16 15:36:47 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LiquorWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
